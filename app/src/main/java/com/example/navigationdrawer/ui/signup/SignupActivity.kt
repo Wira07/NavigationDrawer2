00c77024c48309f473a23a635e6c19479a0c744c
wira07 <wiralodrasaputra07@gmail.com>
2024-01-17 13:50:10 +0700

    Update aja semuanya

--- app/src/main/java/com/example/navigationdrawer/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/ui/signup/SignupActivity.kt
@@ -3,17 +3,24 @@ package com.example.navigationdrawer.ui.signup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
 import com.example.navigationdrawer.MainActivity
 import com.example.navigationdrawer.R
 import com.example.navigationdrawer.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
 
+    private lateinit var topAnim: Animation
     private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Animations
+        topAnim = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.top_animation)
+
+        binding.WiraSukmaSaputra.startAnimation(topAnim)
 
         binding.buttonSign.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
--- app/src/main/java/com/example/navigationdrawer/ui/signup/loginsignup/StartupActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/ui/signup/loginsignup/StartupActivity.kt
@@ -4,19 +4,34 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
+import com.example.navigationdrawer.R
 import com.example.navigationdrawer.databinding.ActivityStartupBinding
 import com.example.navigationdrawer.ui.signup.SignupActivity
 
 class StartupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStartupBinding
+    private lateinit var topAnim: Animation
+    private lateinit var bottomAnim: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStartupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Animations
+        topAnim = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnim = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        binding.imageView.startAnimation(topAnim)
+        binding.WiraSukmaSaputra.startAnimation(bottomAnim)
+        binding.Deskripsi.startAnimation(bottomAnim)
+        binding.loginButton.startAnimation(bottomAnim)
+        binding.signupButton.startAnimation(bottomAnim)
+        binding.Wira2.startAnimation(bottomAnim)
 
 //        binding.loginButton.setOnClickListener {
 //            val loginIntent = Intent(this@StartupActivity, SignupActivity::class.java)
@@ -28,7 +43,6 @@ class StartupActivity : AppCompatActivity() {
             startActivity(signupIntent)
         }
     }
-
     fun callLogin(view: View) {
         val intent = Intent(applicationContext, SignupActivity::class.java)
 
@@ -42,6 +56,4 @@ class StartupActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-
 }
