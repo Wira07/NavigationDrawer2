e96134c7b598b43d90701d9549ab8e16692e24aa
wira07 <wiralodrasaputra07@gmail.com>
2024-01-28 10:14:56 +0700

    Update aja semuanya

--- app/src/main/java/com/example/navigationdrawer/ui/signup/RegistrasiActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/ui/signup/RegistrasiActivity.kt
@@ -1,12 +1,54 @@
 package com.example.navigationdrawer.ui.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Pair
+import com.example.navigationdrawer.MainActivity
 import com.example.navigationdrawer.R
+import com.example.navigationdrawer.databinding.ActivityRegistrasiBinding
 
 class RegistrasiActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrasiBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registrasi)
+        binding = ActivityRegistrasiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val topAnimPair = Pair(binding.gambar1, R.anim.top_animation)
+        val logoNamePair = Pair(binding.logoName, R.anim.top_animation)
+        val Pairfullname = Pair(binding.name, R.anim.top_animation)
+        val Email = Pair(binding.email, R.anim.top_animation)
+        val Phone = Pair(binding.phoneNo, R.anim.top_animation)
+        val sloganNamePair = Pair(binding.sloganName, R.anim.top_animation)
+        val usernamePair = Pair(binding.username, R.anim.bottom_animation)
+        val passwordPair = Pair(binding.password, R.anim.bottom_animation)
+        val buttonSignPair = Pair(binding.buttonSign, R.anim.bottom_animation)
+        val signupPair = Pair(binding.buttonIhave, R.anim.bottom_animation)
+
+        val pairs = arrayOf(
+            topAnimPair,
+            logoNamePair,
+            Pairfullname,
+            Email,
+            Phone,
+            sloganNamePair,
+            usernamePair,
+            passwordPair,
+            buttonSignPair,
+            signupPair
+        )
+
+        pairs.forEach { pair ->
+            pair.first.startAnimation(android.view.animation.AnimationUtils.loadAnimation(this, pair.second))
+        }
+
+        binding.buttonSign.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("kunci", "Nilai yang akan Anda kirim")
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/navigationdrawer/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/ui/signup/SignupActivity.kt
@@ -1,35 +1,44 @@
 package com.example.navigationdrawer.ui.signup
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.Animation
+import android.util.Pair
+import androidx.appcompat.app.AppCompatActivity
 import com.example.navigationdrawer.MainActivity
 import com.example.navigationdrawer.R
 import com.example.navigationdrawer.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
 
-    private lateinit var topAnim: Animation
-    private lateinit var bottomAnim: Animation
     private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Animations
-        topAnim = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.top_animation)
-        bottomAnim = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        val topAnimPair = Pair(binding.gambar1, R.anim.top_animation)
+        val logoNamePair = Pair(binding.logoName, R.anim.top_animation)
+        val sloganNamePair = Pair(binding.sloganName, R.anim.top_animation)
+        val usernamePair = Pair(binding.username, R.anim.bottom_animation)
+        val passwordPair = Pair(binding.password, R.anim.bottom_animation)
+        val forgetPair = Pair(binding.forget, R.anim.bottom_animation)
+        val buttonSignPair = Pair(binding.buttonSign, R.anim.bottom_animation)
+        val signupPair = Pair(binding.signup, R.anim.bottom_animation)
 
-        binding.gambar1.startAnimation(topAnim)
-        binding.logoName.startAnimation(topAnim)
-        binding.sloganName.startAnimation(topAnim)
-        binding.username.startAnimation(bottomAnim)
-        binding.password.startAnimation(bottomAnim)
-        binding.forget.startAnimation(bottomAnim)
-        binding.buttonSign.startAnimation(bottomAnim)
-        binding.signup.startAnimation(bottomAnim)
+        val pairs = arrayOf(
+            topAnimPair,
+            logoNamePair,
+            sloganNamePair,
+            usernamePair,
+            passwordPair,
+            forgetPair,
+            buttonSignPair,
+            signupPair
+        )
+
+        pairs.forEach { pair ->
+            pair.first.startAnimation(android.view.animation.AnimationUtils.loadAnimation(this, pair.second))
+        }
 
         binding.buttonSign.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -37,5 +46,10 @@ class SignupActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.signup.setOnClickListener {
+            val intent = Intent(this, RegistrasiActivity::class.java)
+            intent.putExtra("kunci", "Nilai yang akan Anda kirim")
+            startActivity(intent)
+        }
     }
 }
